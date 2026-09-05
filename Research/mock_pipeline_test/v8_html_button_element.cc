0f33ff92071d5ecaddd7c07344915da71183a0a4
TheStudent00 <the.student.235711@gmail.com>
2026-09-04 22:37:55 -0400

    auto: 3 files (v8_html_button_element.cc, v8_html_button_element.cpp, v8_html_button_element.ledger.json)
    2026-09-04T22:37:55-04:00
    X-Auto-Commit: repo-daemon

--- Research/mock_pipeline_test/v8_html_button_element.cc
+++ Research/mock_pipeline_test/v8_html_button_element.cc
@@ -1,12 +1,8 @@
 #include "third_party/blink/renderer/bindings/core/v8/v8_html_button_element.h"
 #include "v8/include/v8.h"
 
-namespace blink {
-
 // MOCK BINDING: A simple representation of Chromium's DOM-to-V8 bridge.
-void V8HTMLButtonElement::ClickMethodCallback(
-    const v8::FunctionCallbackInfo<v8::Value>& info) {
-    
+void ClickMethodCallback(const v8::FunctionCallbackInfo<v8::Value>& info) {
     v8::Isolate* isolate = info.GetIsolate();
     v8::HandleScope handle_scope(isolate);
     v8::Local<v8::Context> context = isolate->GetCurrentContext();
@@ -14,5 +10,3 @@ void V8HTMLButtonElement::ClickMethodCallback(
     HTMLButtonElement* impl = V8HTMLButtonElement::ToImpl(info.Holder());
     impl->click();
 }
-
-} // namespace blink
