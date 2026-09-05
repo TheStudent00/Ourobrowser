ae62b70313a1cc3ef79af0c5c154cbb225157e2f
TheStudent00 <the.student.235711@gmail.com>
2026-09-04 22:53:55 -0400

    auto: 5 files (DASHBOARD.md, v8_html_button_element.cpp, v8_html_button_element.ledger.json, +2)
    2026-09-04T22:53:55-04:00
    X-Auto-Commit: repo-daemon

--- Research/mock_pipeline_test/v8_html_button_element.cpp
+++ Research/mock_pipeline_test/v8_html_button_element.cpp
@@ -5,10 +5,10 @@
 #include <any>
 #include <stdexcept>
 
-void ClickMethodCallback(v8::FunctionCallbackInfo<v8::Value> info) {
-        v8::Isolate isolate = info.GetIsolate();
-        v8::HandleScope handle_scope = nullptr;
-        v8::Local<v8::Context> context = isolate.GetCurrentContext();
-        HTMLButtonElement impl = V8HTMLButtonElement::ToImpl(info.Holder());
+void ClickMethodCallback(PyObject* info) {
+        PyInterpreterState* isolate = info.GetIsolate();
+        PyObject* handle_scope = nullptr;
+        PyDictObject* context = isolate.GetCurrentContext();
+        HTMLButtonElement* impl = V8HTMLButtonElement::ToImpl(info.Holder());
     impl.click();
 }
