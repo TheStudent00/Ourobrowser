1ec6793b8c24cb4d5d1ff4e4cd1a33e0a1dc46cc
TheStudent00 <TheStudent00@users.noreply.github.com>
2026-09-04 22:52:26 -0400

    auto: 1 file (v8_html_button_element.cpp)
    2026-09-04T22:52:26-04:00
    X-Auto-Commit: repo-daemon

--- Research/mock_pipeline_test/v8_html_button_element.cpp
+++ Research/mock_pipeline_test/v8_html_button_element.cpp
@@ -6,9 +6,9 @@
 #include <stdexcept>
 
 void ClickMethodCallback(v8::FunctionCallbackInfo<v8::Value> info) {
-    v8::Isolate isolate = info.GetIsolate();
-    v8::HandleScope handle_scope = nullptr;
-    v8::Local<v8::Context> context = isolate.GetCurrentContext();
-    HTMLButtonElement impl = V8HTMLButtonElement::ToImpl(info.Holder());
+        v8::Isolate isolate = info.GetIsolate();
+        v8::HandleScope handle_scope = nullptr;
+        v8::Local<v8::Context> context = isolate.GetCurrentContext();
+        HTMLButtonElement impl = V8HTMLButtonElement::ToImpl(info.Holder());
     impl.click();
 }
