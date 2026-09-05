c5bd6c5bd73904e5227946277e2ec71d66cc3282
TheStudent00 <the.student.235711@gmail.com>
2026-09-05 09:28:36 -0400

    auto: 1 file (run_incremental_build.sh)
    2026-09-05T09:28:36-04:00
    X-Auto-Commit: repo-daemon

--- Research/chromium_compilation/run_incremental_build.sh
+++ Research/chromium_compilation/run_incremental_build.sh
@@ -3,7 +3,7 @@ export PATH="/projects/depot_tools:$PATH"
 cd /projects/chromium_src/src
 
 echo "Running ninja... (Saving output to build_log.txt)"
-ninja -C out/Default third_party/blink/renderer/bindings/core/v8 > build_log.txt 2>&1 || true
+ninja -C out/Default blink > build_log.txt 2>&1 || true
 
 echo "Parsing errors..."
 python3 /projects/Ourobrowser/Research/chromium_compilation/ninja_error_parser.py build_log.txt > ninja_errors.log
