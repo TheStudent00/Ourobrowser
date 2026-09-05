35519f61b625644242f8a0ae37f4535241d88ca7
TheStudent00 <TheStudent00@users.noreply.github.com>
2026-09-04 22:37:55 -0400

    auto: 3 files (v8_html_button_element.cc, v8_html_button_element.cpp, v8_html_button_element.ledger.json)
    2026-09-04T22:37:55-04:00
    X-Auto-Commit: repo-daemon

--- Research/mock_pipeline_test/v8_html_button_element.cpp
+++ Research/mock_pipeline_test/v8_html_button_element.cpp
@@ -4,3 +4,11 @@
 #include <unordered_map>
 #include <any>
 #include <stdexcept>
+
+void ClickMethodCallback(v8::FunctionCallbackInfo<v8::Value> info) {
+    v8::Isolate isolate = info.GetIsolate();
+    v8::HandleScope handle_scope = nullptr;
+    v8::Local<v8::Context> context = isolate.GetCurrentContext();
+    HTMLButtonElement impl = V8HTMLButtonElement::ToImpl(info.Holder());
+    impl.click();
+}
