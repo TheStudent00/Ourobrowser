7fc9a6cd8f4586072fc7792d32b4012e555e7e92
TheStudent00 <the.student.235711@gmail.com>
2026-09-04 22:51:55 -0400

    auto: 1 file (mock_test_runner.sh)
    2026-09-04T22:51:55-04:00
    X-Auto-Commit: repo-daemon

--- Research/mock_pipeline_test/mock_test_runner.sh
+++ Research/mock_pipeline_test/mock_test_runner.sh
@@ -3,7 +3,7 @@ set -e
 
 MOCK_FILE="v8_html_button_element.cc"
 
-PC_V3_DIR="$HOME/Programming/0_Archive/PseudoCoup_v3"
+PC_V3_DIR="$HOME/Programming/Ourobrowser/Tools/PCv3.1"
 
 echo "=== Running PseudoCoup V3 (Mock) ==="
 PYTHONPATH="$PC_V3_DIR" python3 -m pseudocoup.cli \
