b5b866182fda2636f96fae84b9df731f7e1bbd2a
TheStudent00 <TheStudent00@users.noreply.github.com>
2026-09-04 23:13:56 -0400

    auto: 1 file (run_incremental_build.sh)
    2026-09-04T23:13:56-04:00
    X-Auto-Commit: repo-daemon

--- Research/chromium_compilation/run_incremental_build.sh
+++ Research/chromium_compilation/run_incremental_build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
+export PATH="/projects/Ourobrowser/Research/chromium_compilation/depot_tools:$PATH"
 export PATH="$HOME/Programming/depot_tools:$PATH"
-cd ~/Programming/chromium_src/src
-
-echo "Running ninja... (Saving output to build_log.txt)"
-ninja -C out/Default third_party/blink/renderer/bindings/core/v8 > build_log.txt 2>&1 || true
-
-echo "Parsing errors..."
-python3 ~/Programming/Ourobrowser/Research/chromium_compilation/ninja_error_parser.py build_log.txt
+export PATH="/projects/chromium_src/depot_tools:$PATH"
+# Wait, I didn't map depot_tools to the container!
