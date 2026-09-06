11be1ce66cd90a5feffb148c2d9b4cbbcedcf9a6
TheStudent00 <the.student.235711@gmail.com>
2026-09-05 20:33:28 -0400

    auto: 2 files (parse_test4.py, patch_ingress_fun.py)
    2026-09-05T20:33:28-04:00
    X-Auto-Commit: repo-daemon

--- parse_test4.py
+++ parse_test4.py
@@ -4,4 +4,4 @@ from tree_sitter import Parser, Language
 parser = Parser(Language(ts.language()))
 src = b"void f() { words_[0]; }"
 tree = parser.parse(src)
-print(tree.root_node.sexp())
+print(tree.root_node)
