a52b0f7da6ff458ad3214e5c448fa789ab2167ac
TheStudent00 <TheStudent00@users.noreply.github.com>
2026-09-06 01:42:00 -0400

    auto: 54 files (expand_depth.py, index_test.html, mark_done.py, +51)
    2026-09-06T01:42:00-04:00
    X-Auto-Commit: repo-daemon

--- parse_test5.py
+++
@@ -1,7 +0,0 @@
-import tree_sitter_cpp as ts
-from tree_sitter import Parser, Language
-
-parser = Parser(Language(ts.language()))
-src = b"void f() { uint64_t raw = words_[0]; }"
-tree = parser.parse(src)
-print(tree.root_node)
--- parse_test6.py
+++
@@ -1,7 +0,0 @@
-import tree_sitter_cpp as ts
-from tree_sitter import Parser, Language
-
-parser = Parser(Language(ts.language()))
-src = b"uint64_t raw = words_[0];"
-tree = parser.parse(src)
-print(tree.root_node)
