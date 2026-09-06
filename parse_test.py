ee5d4630c1f2d196f6646637fff3a5cfaddb8b8d
TheStudent00 <the.student.235711@gmail.com>
2026-09-06 01:42:00 -0400

    auto: 54 files (expand_depth.py, index_test.html, mark_done.py, +51)
    2026-09-06T01:42:00-04:00
    X-Auto-Commit: repo-daemon

--- parse_test.py
+++
@@ -1,7 +0,0 @@
-import tree_sitter_cpp as ts
-from tree_sitter import Parser, Language
-
-parser = Parser(Language(ts.language()))
-src = b"void BigInt::Words::empty() { return; }"
-tree = parser.parse(src)
-print(tree.root_node)
--- parse_test3.py
+++
@@ -1,7 +0,0 @@
-import tree_sitter_cpp as ts
-from tree_sitter import Parser, Language
-
-parser = Parser(Language(ts.language()))
-src = b"void f() { cond ? a : b; }"
-tree = parser.parse(src)
-print(tree.root_node)
