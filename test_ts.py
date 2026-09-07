804c05745fe2268a5da6e8e57416c883a68b3129
TheStudent00 <TheStudent00@users.noreply.github.com>
2026-09-07 02:04:05 -0400

    auto: 1 file (test_ts.py)
    2026-09-07T02:04:05-04:00
    X-Auto-Commit: repo-daemon

--- test_ts.py
+++ test_ts.py
@@ -1,13 +1,10 @@
 import tree_sitter_cpp as ts
 from tree_sitter import Language, Parser
-
 parser = Parser(Language(ts.language()))
-
-src = b"LocalWindowProxy::LocalWindowProxy(Isolate* isolate, LocalFrame& frame) : WindowProxy(isolate, frame) {}"
+src = b"class A { A(const A&) = delete; };"
 tree = parser.parse(src)
 def print_node(node, indent=0):
     print("  "*indent + node.type + (" ("+node.type+")" if node.is_named else ""))
     for child in node.children:
         print_node(child, indent+1)
-
 print_node(tree.root_node)
