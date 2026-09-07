af2408e55e0e53031973189b1616e9d3b355d018
TheStudent00 <TheStudent00@users.noreply.github.com>
2026-09-07 01:58:35 -0400

    auto: 1 file (test_ts.py)
    2026-09-07T01:58:35-04:00
    X-Auto-Commit: repo-daemon

---
+++ test_ts.py
@@ -0,0 +1,13 @@
+import tree_sitter_cpp as ts
+from tree_sitter import Language, Parser
+
+parser = Parser(Language(ts.language()))
+
+src = b"void foo() { if (auto* iframe = DynamicTo<HTMLIFrameElement>(*element)) {} }"
+tree = parser.parse(src)
+def print_node(node, indent=0):
+    print("  "*indent + node.type + (" ("+node.type+")" if node.is_named else ""))
+    for child in node.children:
+        print_node(child, indent+1)
+
+print_node(tree.root_node)
